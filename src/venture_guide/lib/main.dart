4f1d650935978fa69545ed53c07fc8795475be48
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2024-03-09 13:40:51 -0300

    Create and plug to firebase

--- src/venture_guide/lib/main.dart
+++ src/venture_guide/lib/main.dart
@@ -1,16 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import 'package:venture_guide/app/injector.dart';
 import 'package:venture_guide/app/map/domain/services/marker_service.dart';
 import 'package:venture_guide/app/map/presentation/pages/home_page/home_page_widget.dart';
 import 'package:wakelock/wakelock.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();  
+  WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
   await FlutterMapTileCaching.initialise();
   await FMTC.instance('mapStore').manage.createAsync();
   Wakelock.enable();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -23,11 +28,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    getIt<MarkerService>().syncMarkers().then((value) => null);
 
-    getIt<MarkerService>()
-    .syncMarkers()
-    .then((value) => null);
-    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
--- src/venture_guide/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/venture_guide/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import geolocator_apple
 import isar_flutter_libs
 import path_provider_foundation
@@ -12,6 +13,7 @@ import sqflite
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
